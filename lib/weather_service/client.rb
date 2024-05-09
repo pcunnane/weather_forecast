b9f7fc343a29bb2add46c5e48e85b574a0b7a07f
Patrick Cunnane <pat@letote.com>
2024-05-09 16:08:44 -0700

    30 minute caching in memory, add notice to UI

--- lib/weather_service/client.rb
+++ lib/weather_service/client.rb
@@ -2,17 +2,23 @@ module WeatherService
   module Client
     class Error < StandardError; end
 
+    CACHE_TIME = 30.minutes
+
     class << self
       def current_by_zipcode(zipcode)
-        api_response = api_client.current_zip(zipcode, 'US')
-        create_response(api_response)
+        cached(zipcode) do
+          api_response = api_client.current_zip(zipcode, 'US')
+          create_response(api_response)
+        end
       rescue Exception => e
         raise Error.new(e.message)
       end
 
       def current_by_city_state(city, state)
-        api_response = api_client.current_city(city, state, 'US')
-        create_response(api_response)
+        cached(city + state) do
+          api_response = api_client.current_city(city, state, 'US')
+          create_response(api_response)
+        end
       rescue Exception => e
         raise Error.new(e.message)
       end
@@ -21,6 +27,19 @@ module WeatherService
 
       def api_client
         @api_client ||= OpenWeather::Client.new
+      end
+
+      def cached(key)
+        key = "weather_for_#{key}"
+        response = Rails.cache.read(key)
+        if response
+          response.cached = true
+          return response
+        end
+
+        response = yield
+        Rails.cache.write(key, response, expires_in: CACHE_TIME)
+        response
       end
 
       def create_response(api_response)
--- lib/weather_service/response.rb
+++ lib/weather_service/response.rb
@@ -3,5 +3,10 @@ module WeatherService
     attr_accessor :temperature
     attr_accessor :min_temperature
     attr_accessor :max_temperature
+    attr_writer :cached
+
+    def cached?
+      @cached
+    end
   end
 end
